225ad035f07231de284b6746436ad11c7defb2d3
i.bogdanova <irene.bogdanova.95@gmail.com>
2021-12-13 21:45:25 +0300

    Initial commit

---
+++ backend/src/main/java/com/bogdanova/bookstore/config/HibernateConfiguration.java
@@ -0,0 +1,57 @@
+package com.bogdanova.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfiguration {
+
+    @Value("${db.url}")
+    private String dbUrl;
+    @Value("${db.driver}")
+    private String driver;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${hibernate.dialect}")
+    private String dialect;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", dialect);
+        sessionFactory.setHibernateProperties(hibernateProperties);
+        return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        transactionManager.setNestedTransactionAllowed(true);
+        return transactionManager;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Java(Spring, Hibernate)-Angular simple CRUD application
+## In order to run this application, you need to install:
+
+ - Docker
